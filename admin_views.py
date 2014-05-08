8c25a1dc6806883137e797b4e707257eacbf1875
Michael Garrido <michael.a.garrido@gmail.com>
2014-05-07 23:15:27 -0700

    prep auth filter for all admin views

--- admin_views.py
+++ admin_views.py
@@ -1,9 +1,11 @@
 from flask.ext.admin import Admin, BaseView, expose
 from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.login import login_user, logout_user, current_user, login_required
 
 # TODO add @login_required for all admin views
 
 class MyView(BaseView):
+    #@login_required
     @expose('/')
     def index(self):
         return self.render('_admin_home.html')
@@ -14,6 +16,7 @@ class MyView(BaseView):
 # class ModelView(model, session, name=None, category=None, endpoint=None, url=None)
 
 class ProjectView(ModelView):
+    #@login_required
     list_template = 'admin/project_list.html'
     #edit_template = 'my_edit_template.html'
     #create_template = 'create.html'
@@ -22,8 +25,131 @@ class ProjectView(ModelView):
 
 
 def add_admin_views(admin,models,db):
-  admin.add_view(MyView(name='Hello'))
+  #admin.add_view(MyView(name='Hello'))
 
   admin.add_view(ProjectView(models.Project, db.session))
   admin.add_view(ModelView(models.Page, db.session))
   admin.add_view(ModelView(models.User, db.session))
+
+  ####
+  #https://github.com/mrjoes/flask-admin/blob/master/examples/auth/auth.py
+'''
+import os
+from flask import Flask, url_for, redirect, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from wtforms import form, fields, validators
+from flask.ext import admin, login
+from flask.ext.admin.contrib import sqla
+from flask.ext.admin import helpers, expose
+
+
+  # Define login and registration forms (for flask-login)
+class LoginForm(form.Form):
+    login = fields.TextField(validators=[validators.required()])
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise validators.ValidationError('Invalid user')
+
+        if user.password != self.password.data:
+            raise validators.ValidationError('Invalid password')
+
+    def get_user(self):
+        return db.session.query(User).filter_by(login=self.login.data).first()
+
+
+class RegistrationForm(form.Form):
+    login = fields.TextField(validators=[validators.required()])
+    email = fields.TextField()
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_login(self, field):
+        if db.session.query(User).filter_by(login=self.login.data).count() > 0:
+            raise validators.ValidationError('Duplicate username')
+
+
+# Initialize flask-login
+def init_login():
+    login_manager = login.LoginManager()
+    login_manager.init_app(app)
+
+    # Create user loader function
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.query(User).get(user_id)
+
+
+# Create customized model view class
+class MyModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        return login.current_user.is_authenticated()
+
+
+# Create customized index view class that handles login & registration
+class MyAdminIndexView(admin.AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not login.current_user.is_authenticated():
+            return redirect(url_for('.login_view'))
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/login/', methods=('GET', 'POST'))
+    def login_view(self):
+        # handle user login
+        form = LoginForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = form.get_user()
+            login.login_user(user)
+
+        if login.current_user.is_authenticated():
+            return redirect(url_for('.index'))
+        link = '<p>Don\'t have an account? <a href="' + url_for('.register_view') + '">Click here to register.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/register/', methods=('GET', 'POST'))
+    def register_view(self):
+        form = RegistrationForm(request.form)
+        if helpers.validate_form_on_submit(form):
+            user = User()
+
+            form.populate_obj(user)
+
+            db.session.add(user)
+            db.session.commit()
+
+            login.login_user(user)
+            return redirect(url_for('.index'))
+        link = '<p>Already have an account? <a href="' + url_for('.login_view') + '">Click here to log in.</a></p>'
+        self._template_args['form'] = form
+        self._template_args['link'] = link
+        return super(MyAdminIndexView, self).index()
+
+    @expose('/logout/')
+    def logout_view(self):
+        login.logout_user()
+        return redirect(url_for('.index'))
+
+
+# Flask views
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Initialize flask-login
+init_login()
+
+# Create admin
+admin = admin.Admin(app, 'Auth', index_view=MyAdminIndexView(), base_template='my_master.html')
+
+# Add view
+admin.add_view(MyModelView(User, db.session))
+
+'''
